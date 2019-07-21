13e410ebb62fabd187e10cb4707e1f40fcf75b04
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-07-21 14:06:10 +0300

    Добавил в темплейт примеры валидаторов

--- src/datasets/_TEMPLATE.js
+++ src/datasets/_TEMPLATE.js
@@ -2,16 +2,21 @@
 //  Заменив его на название табилы (или другое логическое) автоматически происходит генерация небоходимого функционала
 //  Предварительно необходимо описать полученые автоматически поля, правильно прописать типы и рассатавить это все по Filter, List и другим 
 //  Не нужный функционал закоментировать
+
 import React from 'react';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import { Tab } from '@material-ui/core';
 import { withStyles } from '@material-ui/core';
+
+// Не взлетело
 // import ChevronLeft from '@material-ui/icons/ChevronLeft';
 // import ChevronRight from '@material-ui/icons/ChevronRight';
 // import Toolbar from '@material-ui/core/Toolbar';
 // import Typography from '@material-ui/core/Typography';
 // import ResetViewsButton from '../service/ResetViewsButton';
+
+// импорт всяких полей
 import {
     Pagination,
     CreateButton, 
@@ -42,6 +47,19 @@ import {
     Filter,
     TabbedForm,
     FormTab
+} from 'react-admin';
+
+// Импорт валидации
+import {
+    required,
+    minLength,
+    maxLength,
+    minValue,
+    maxValue,
+    number,
+    regex,
+    email,
+    choices
 } from 'react-admin';
 
 // кастомный пагинатор, Для маленьких списков
@@ -225,6 +243,18 @@ const TemplateCreateToolbar = props => (
         />
     </Toolbar>
 );
+
+// Примеры валидаторов
+// const validateFirstName = [required(), minLength(2), maxLength(15)];
+// const validateEmail = email();
+// const validateAge = [number(), minValue(18)];
+// const validateZipCode = regex(/^\d{5}$/, 'Must be a valid Zip Code');
+// const validateSex = choices(['m', 'f'], 'Must be Male or Female');
+
+// Пример вставки валидатора на поле
+{/* <TextInput label="First Name" source="firstName" validate={validateFirstName} /> */}
+
+
 
 export const TemplateCreate = ({ classes, ...props }) => (
     <div>
